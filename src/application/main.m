a24009fad6eace65dd8c2b4748498fe2a28ebc56
Jeff Dlouhy <jdlouhy@cray.resnet.neu.edu>
2008-07-09 23:33:27 -0400

    Initial commit

---
+++ src/application/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Corripio
+//
+//  Created by Jeff Dlouhy on 7/9/08.
+//  Copyright nClass Software 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
